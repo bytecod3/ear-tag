e5d703fbed5114ccb0dd3d75d2b05a0f5adf7127
bytecod3 <emwiti658@gmail.com>
2025-02-10 05:59:28 +0300

    adds acceleration code-template

---
+++ firmware/activity-monitoring/code/include/defines.h
@@ -0,0 +1,6 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define SERIAL_BAUDRATE 115200
+
+#endif // DEFINES_H
---
+++ firmware/activity-monitoring/code/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include "mpu.h"
+
+/* create MPU6050 class instance */
+
+void setup() {
+    Serial.begin(SERIAL_BAUDRATE);
+
+    /* initialize mpu6050 in interrupt mode */
+
+
+
+}
+
+
+void loop() {
+
+    /* mpu 6050 */
+
+    
+
+}
